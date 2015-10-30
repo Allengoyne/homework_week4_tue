8958f711e3bd6a7c0774455a2bd7aa1209abffdf
Allen Goyne <allen.goyne@gmail.com>
2015-10-30 14:50:09 -0500

    commit ahead of sidebar styling changes

--- main.js
+++ main.js
@@ -2,38 +2,37 @@
 $.getJSON('https://api.github.com/users/allengoyne/repos').done(function(data, status, xhrObject) {
 
   console.log(data);
-// displays icon nav in header
-  var navContainer = $('.iconNav');
-  data.forEach(function(navIcon){
-    var notificationsElement = $('<div class="notificationsIcon"><span class="octicon octicon-bell"></span>' + navIcon.notifications_url + '</div>');
-
-    // iconsNavElement.append()
-
-  });
-
 
   // displays all repos
   var reposContainer = $('.repos');
   data.forEach(function(repo) {
     var repoElement = $('<div class="repoName">' + repo.name + '</div>');
-    var forksElement = $('<div class="repoForks"><span class="octicon octicon-repo-forked"></span>'+ repo.forks_count + '</div>');
     var updateElement = $('<div class="repoUpdate">' + '<div> updated at: </div>' + repo.updated_at + '</div>');
+    var forksElement = $('<div class="repoForks"><span class="octicon octicon-repo-forked"></span>'+ repo.forks_count + '</div>');
     var languageElement = $('<div class="repoLanguage">' + repo.language + '</div>');
     var starsElement = $('<div class="repoStars"><span class="octicon octicon-star"></span>' + repo.stargazers_count + '</div>');
 
+    repoElement.append(updateElement);
     repoElement.append(starsElement);
     repoElement.append(languageElement);
     repoElement.append(forksElement);
-    repoElement.append(updateElement);
     reposContainer.append(repoElement);
   });
 
 })
 
-// get profile info
+// Get Profile info
 $.getJSON('https://api.github.com/users/allengoyne').done(function(data, status, xhrObject) {
   var avatar = data.avatar_url;
 
+  // Display Profile info in sidebar
+  // var profileContainer = $('.sidebar');
+  // data.forEach(function(profile) {
+
+  // var nameElement = $('<div>')
+
   console.log(data);
+
   $(".avatar").attr("src", avatar );
+  // $(".sidebar").attr("src", avatar );
 })
