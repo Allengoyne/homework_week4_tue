df1ea82bc15615fbc45961470d4b76eb3fef5e0b
Allen Goyne <allen.goyne@gmail.com>
2015-11-03 07:37:49 -0600

    initial commit

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
 // get the repos
 $.getJSON('https://api.github.com/users/allengoyne/repos').done(function(data, status, xhrObject) {
 
-  console.log(data);
 
   // displays all repos
-  var reposContainer = $('.repos');
-  data.forEach(function(repo) {
-    var repoElement = $('<div class="repoName">' + repo.name + '</div>');
-    var updateElement = $('<div class="repoUpdate">' + '<div> updated at: </div>' + repo.updated_at + '</div>');
-    var forksElement = $('<div class="repoForks"><span class="octicon octicon-repo-forked"></span>'+ repo.forks_count + '</div>');
-    var languageElement = $('<div class="repoLanguage">' + repo.language + '</div>');
-    var starsElement = $('<div class="repoStars"><span class="octicon octicon-star"></span>' + repo.stargazers_count + '</div>');
 
-    repoElement.append(updateElement);
-    repoElement.append(starsElement);
-    repoElement.append(languageElement);
-    repoElement.append(forksElement);
-    reposContainer.append(repoElement);
+
+  // var reposContainer = $('.repos');
+  // data.forEach(function(repo) {
+  //   var repoElement = $('<div class="repoName">' + repo.name + '</div>');
+  //   var updateElement = $('<div class="repoUpdate">' + '<div> updated at: </div>' + repo.updated_at + '</div>');
+  //   var forksElement = $('<div class="repoForks"><span class="octicon octicon-repo-forked"></span>'+ repo.forks_count + '</div>');
+  //   var languageElement = $('<div class="repoLanguage">' + repo.language + '</div>');
+  //   var starsElement = $('<div class="repoStars"><span class="octicon octicon-star"></span>' + repo.stargazers_count + '</div>');
+  //
+  //   repoElement.append(updateElement);
+  //   repoElement.append(starsElement);
+  //   repoElement.append(languageElement);
+  //   repoElement.append(forksElement);
+  //   reposContainer.append(repoElement);
+  // });
+  console.log(data);
+
+  var reposContainer = $('.repos');
+  var reposAll = $('#reposAll')._forEach(function(repo){
+    var reposTemplate = _.template(reposAll);
+    var reposMarkup = reposTemplate(data);
   });
 
+
+  reposContainer.html(reposMarkup);
 })
 
 // Get Profile info
-$.getJSON('https://api.github.com/users/allengoyne').done(function(data, status, xhrObject) {
-  var avatar = data.avatar_url;
-
+$.getJSON('https://api.github.com/users/allengoyne').done(function(data) {
   // Display Profile info in sidebar
-  // var profileContainer = $('.sidebar');
-  // data.forEach(function(profile) {
-
-  // var nameElement = $('<div>')
-
   console.log(data);
+  var profileContainer = $( '.sidebar');
+  var details = $('#details').html();
+  var sidebarTemplate = _.template(details);
+  var html = sidebarTemplate(data);
 
-  $(".avatar").attr("src", avatar );
-  // $(".sidebar").attr("src", avatar );
+  profileContainer.html(html);
 })
